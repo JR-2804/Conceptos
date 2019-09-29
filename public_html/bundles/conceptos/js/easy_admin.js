114d8d5be408bb8edfc9f9e068584fdb8190fbec
JR <josequintana2804@gmail.com>
2019-09-29 15:02:17 -0400

    Increase search bar width in administration

--- public_html/bundles/conceptos/js/easy_admin.js
+++ public_html/bundles/conceptos/js/easy_admin.js
@@ -1,7 +1,16 @@
+function IncreaseSearchBarSize() {
+  $("#easyadmin-list-Product > div > div > section.content-header > div > div:nth-child(1)").removeClass();
+  $("#easyadmin-list-Product > div > div > section.content-header > div > div:nth-child(1)").addClass("col-2");
+  $("#easyadmin-list-Product > div > div > section.content-header > div > div:nth-child(2)").removeClass();
+  $("#easyadmin-list-Product > div > div > section.content-header > div > div:nth-child(2)").addClass("col-10");
+}
+
 $(document).ready(function () {
-    if ($('#flash-messages').length > 0) {
-        setTimeout(function () {
-            $('#flash-messages').hide('slow');
-        }, 5000);
-    }
+  if ($('#flash-messages').length > 0) {
+    setTimeout(function () {
+      $('#flash-messages').hide('slow');
+    }, 5000);
+  }
+
+  IncreaseSearchBarSize()
 });
