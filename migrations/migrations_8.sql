4617c8679f7722b9270a363f064002f5a7c1c7b0
JR <josequintana2804@gmail.com>
2019-12-10 10:04:56 -0500

    Fix migration file

--- migrations/migrations_8.sql
+++ migrations/migrations_8.sql
@@ -4,7 +4,7 @@ ALTER TABLE `request` ADD COLUMN `cuc_extra` double NOT NULL AFTER `two_step_ext
 ALTER TABLE `facture` ADD COLUMN `two_step_extra` double NOT NULL AFTER `pre_facture_id`;
 ALTER TABLE `facture` ADD COLUMN `cuc_extra` double NOT NULL AFTER `two_step_extra`;
 
-ALTER TABLE `pre_facture` ADD COLUMN `twoStepExtra` double NOT NULL AFTER `request_id`;
+ALTER TABLE `pre_facture` ADD COLUMN `two_step_extra` double NOT NULL AFTER `request_id`;
 ALTER TABLE `pre_facture` ADD COLUMN `cuc_extra` double NOT NULL AFTER `two_step_extra`;
 
 ALTER TABLE `member` ADD COLUMN `date`  date NOT NULL AFTER `balance`;
