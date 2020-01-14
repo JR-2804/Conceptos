9627d422a0b614c1ff1245da167708adf1ef898d
landy <14ndy15@gmail.com>
2020-01-14 18:19:00 -0500

    Fix navbar height

--- assets/scripts/modules/app/_search_bar.js
+++ assets/scripts/modules/app/_search_bar.js
@@ -2,13 +2,36 @@ import Headroom  from 'headroom.js';
 
 class SearchBar{
     constructor(){
+        this.body = document.querySelector('body');
+        this.navbar = document.querySelector('.navbar');
+        this.navbarSupportedContent = document.querySelector('.conceptos-navbar #navbarSupportedContent');
+        this.categoriesMenu = document.querySelector('#categories-menu');
+        this.modalCart = document.querySelector('#cartPreviewModal .modal-dialog');
+        this.productDialog = document.querySelector("#filterProductsModal .modal-dialog");
         this.searchBar = document.querySelector(".search_bar");
+
         this.init();
     }
 
     init(){
-        var headroom  = new Headroom(this.searchBar);
+        let headroom  = new Headroom(this.searchBar);
         headroom.init();
+
+        let topDisplacement = this.searchBar.clientHeight + this.navbar.clientHeight;
+        let navbarDisplacement = this.navbar.clientHeight;
+
+        this.body.setAttribute('style', 'top: '+topDisplacement+'px');
+        this.searchBar.setAttribute('style', 'top: '+navbarDisplacement+'px');
+        this.navbarSupportedContent.setAttribute('style', 'top: '+topDisplacement+'px');
+        this.categoriesMenu.setAttribute('style', 'top: '+navbarDisplacement+'px');
+        this.modalCart.setAttribute('style', 'top: '+navbarDisplacement+'px');
+
+        if (this.productDialog !== null)
+            this.productDialog.setAttribute('style', 'top: '+navbarDisplacement+'px');
+    }
+
+    spaces(){
+
     }
 }
 
