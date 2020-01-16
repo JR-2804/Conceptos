c1902c7840f9afbaaa34cbc0caf54ecfb261f701
landy <14ndy15@gmail.com>
2020-01-15 20:06:31 -0500

    cookie for show memebership card only once and offer image

---
+++ assets/scripts/modules/app/_utils.js
@@ -0,0 +1,11 @@
+export function setCookie(value, name='products_cart' , days=3) {
+    let date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    let expires = "; expires="+date.toGMTString ();
+    document.cookie = name + "=" + value +
+        expires + "; path=/";
+}
+
+export function getCookie(name) {
+    return  document.cookie.includes(name);
+}
--- assets/scripts/modules/index/_membership_card.js
+++ assets/scripts/modules/index/_membership_card.js
@@ -1,10 +1,22 @@
+import { setCookie, getCookie } from '../app/_utils';
+
 class MembershipCard {
     constructor(){
         this.memebershipCard = document.querySelector('.membership_card');
         this.memebershipCardClose = document.querySelector('.membership_card__close');
 
-        this.event();
+        this.showOrNot();
     }
+
+    showOrNot(){
+        if (getCookie('membershipCard'))
+            this.memebershipCard.classList.add('membership_card--no-display');
+        else {
+            setCookie('membershipCard', '1');
+            this.event();
+        }
+    }
+
     event(){
         setTimeout(this.hideCard.bind(this), 20000);
         this.memebershipCardClose.addEventListener('click', this.hideCard.bind(this));
