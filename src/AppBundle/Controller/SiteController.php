629a5c1967fd947201d70e020531d63b2c443070
JR <josequintana2804@gmail.com>
2020-03-04 07:16:50 -0500

    Fix bugs

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -1286,7 +1286,7 @@ class SiteController extends Controller
             'forClient' => false,
         ]);
 
-        return $this->render(':site:request-email.html.twig', [
+        $bodyClient = $this->renderView(':site:request-email.html.twig', [
             'request' => $requestDB,
             'inStore' => $inStore,
             'home' => $home,
