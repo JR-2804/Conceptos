18fd04e0b9d0150bf7c8ac46d73ce3ca58a296b1
landy <14ndy15@gmail.com>
2020-05-16 18:28:43 -0400

    small whatsapp style corr

--- assets/scripts/modules/app/_utils.js
+++ assets/scripts/modules/app/_utils.js
@@ -1,6 +1,14 @@
 export function setCookie(value, name='products_cart' , days=1) {
     let date = new Date();
-    date.setTime(date.getTime()+(days*24*60*60*1000));
+
+    if (days === -1){
+        let hours = 24 - date.getHours();
+        date.setTime(date.getTime()+(hours*60*60*1000));
+    }
+    else{
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+    }
+
     let expires = "; expires="+date.toGMTString ();
     document.cookie = name + "=" + value + expires + "; path=/";
 }
