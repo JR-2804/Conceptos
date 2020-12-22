189d971e3fd78917891cb49b37d780bc154703cd
JR <josequintana2804@gmail.com>
2020-12-21 22:25:20 -0500

    ✨ Implement migrations to fix categories

---
+++ migrations/migrations_33.sql
@@ -0,0 +1,10 @@
+INSERT INTO category_parent VALUES(145, 13);
+INSERT INTO category_parent VALUES(29, 20);
+INSERT INTO category_parent VALUES(29, 77);
+INSERT INTO category_parent VALUES(21, 144);
+
+DELETE FROM category WHERE category.id = 169;
+
+INSERT INTO category_parent VALUES(170, 11);
+INSERT INTO category_parent VALUES(61, 11);
+INSERT INTO category_parent VALUES(21, 9);
